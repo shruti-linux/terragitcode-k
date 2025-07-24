1ae23631509cef10d8915972d84bb36897c58b9c
shruti-linux <131751004+shruti-linux@users.noreply.github.com>
2025-07-24 15:53:43 +0400

    Update main.tf

--- terraform-ansible/main.tf
+++ terraform-ansible/main.tf
@@ -1,7 +1,7 @@
 provider "google" {
   project     = "testjune13-462806"
   region      = "us-central1"
-  credentials = file("~/sec.json")
+  credentials = file("/home/shruti/sec.json")
 }
 
 #tf-state-prod-bykumar need to create manully
